bba24d923c7c69fa609c5f6b5faf0d3942654506
Shajeed <shajeed@live.fr>
2022-08-16 09:17:49 -0400

    updated app.js

--- App.js
+++ App.js
@@ -1,19 +1,4 @@
 // import { StatusBar } from 'expo-status-bar'
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  View,
-  Image,
-  Alert,
-  StatusBar,
-  Dimensions,
-  Platform,
-  SafeAreaView,
-  Button,
-  ImageBackground,
-} from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
