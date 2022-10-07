42e2f666457ce61b7a2b43ec8a1a1596fd42d168
Shajeed <shajeed0@gmail.com>
2022-10-06 21:42:14 -0400

    small upadate

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-// import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react';
 import TestScreen from './app/screens/testScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
 import MessagesScreen from './app/screens/MessagesScreen';
 import SafeScreen from './app/components/SafeScreen';
 import Icon from './app/components/Icon';
@@ -15,14 +15,28 @@ import ListingScreen from './app/screens/ListingScreen';
 
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+
+
   return (
-    // <View style={styles.container} >
-    //   <AppButton value='Login' onPress={() => console.log("Button Pressed")} />
-    // </View>
+
     // <MessagesScreen />
     // <AccountScreen />
-    <ListingScreen />
+    // <ListingScreen />
 
+    <SafeScreen>
+      <Text>{firstName}</Text>
+      <TextInput 
+        maxLength={5}
+        keyboardType="numeric"
+        onChangeText={text => setFirstName(text)}
+        placeholder='First Name' 
+        style={{
+          borderBottomColor: '#ccc',
+          borderBottomWidth: 1,
+        }}
+      />
+    </SafeScreen>
   );
 }
 
